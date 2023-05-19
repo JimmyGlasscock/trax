b4b5bbe26c5d905bda4eba628947d27fb09cd9de
Jimmy Glasscock <jamesglasscock99@gmail.com>
2023-05-19 16:42:34 -0600

    added migration and model for Car and Trip

---
+++ app/Trip.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Trip extends Model
+{
+    use HasFactory;
+}
---
+++ database/migrations/2023_05_19_223919_create_trips_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trips', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('car_id');
+            $table->date('date');
+            $table->integer('miles');
+            $table->timestamps();
+
+            $table->foreign('car_id')
+               ->references('id')
+               ->on('cars')
+               ->onUpdate('cascade')
+               ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trips');
+    }
+};
